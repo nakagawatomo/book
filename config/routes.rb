8acfdbb1c910f955a2e7501bc2ffc3b2dbcd8a0d
nakagawatomo <132278131+nakagawatomo@users.noreply.github.com>
2023-05-14 22:09:19 +0000

    book

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+  get '/top' => 'homes#top'
+
+  post 'lists' => 'lists#create'
+
+  #get 'lists' => 'lists#new'
+  get '/new' => 'lists#new'
+  get 'lists/index'
+
+  get 'lists/show'
+  get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
