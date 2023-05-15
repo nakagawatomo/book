46198eed1c1dc4bbed8cd838c2ab76da8d03adde
nakagawatomo <132278131+nakagawatomo@users.noreply.github.com>
2023-05-15 05:22:47 +0000

    book3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,20 @@
 class ListsController < ApplicationController
+
+
   def index
     @list = List.new
     @lists = List.all
   end
 
 def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to list_path(@list.id)
+    else
+      @lists = List.all
+      render :index
+    end
   end
 
 
@@ -21,14 +28,20 @@ def create
 
 
   def update
-    list = List.find(params[:id])
-    list.update(list_params)
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to list_path(list.id)
+    else
+      @lists = List.all
+      render :show
+    end
   end
 
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/index'  # 投稿一覧画面へリダイレクト
   end
 
